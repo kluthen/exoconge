d8741143d43c160765ed0ea8ad52647daefb4d07
Bastien Barcaioni <bastien.barcaioni@gmail.com>
2026-03-18 14:10:27 +0000

    maj objectifs

--- lib/engine.ts
+++ lib/engine.ts
@@ -6,11 +6,23 @@ export class Engine {
 
 export function ComputeNbHolidays(emp: Employee, begin :Date, end: Date): [boolean/* OK*/, number /* nb half day consumed*/, DoW[]/*majored consummed*/]{
     // Phase 1: voler des slips
+    // Compter le nombre de demi journée
+    // virer les dimanches, jour fériers, jour non travaillé par le gus.
+
+    // Décision: Mode majoré ou pas ?
+    if(true/*pas majoré*/)
+    {
+        return [true, 0, []]
+    }
 
 
     // Phase 2: ????
 
 
+
+    // detection des jours majorable
+    // pour chaque jour majorable on verifie le décompte déjà majoré
+    // on annote le nombre de nouvelle majorations & augmente le calcule de demi journée consommées.
     // Phase 3: Profits!!!
     return [true, 0, []]
 }
