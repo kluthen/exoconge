c85f1d4b97972d1774e47093b2e48385bacac303
Bastien Barcaioni <bastien.barcaioni@gmail.com>
2026-03-18 14:06:30 +0000

    toto

--- lib/engine.ts
+++ lib/engine.ts
@@ -5,5 +5,12 @@ export class Engine {
 }
 
 export function ComputeNbHolidays(emp: Employee, begin :Date, end: Date): [boolean/* OK*/, number /* nb half day consumed*/, DoW[]/*majored consummed*/]{
-    
+    // Phase 1: voler des slips
+
+
+    // Phase 2: ????
+
+
+    // Phase 3: Profits!!!
+    return [true, 0, []]
 }
